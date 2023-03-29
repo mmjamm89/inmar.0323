9a0f6c28aeda1ae7f705ef84de73ee4df05bcc14
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2023-03-28 22:49:54 -0300

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.add('show');
         } else {
             entry.target.classList.remove('show');
-        }
+        }                               
     });
 });
 
