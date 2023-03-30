f19e344c27b02df625a88917fd24c5237431288a
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2023-03-29 23:57:26 -0300

    producto destacado

--- form.js
+++ form.js
@@ -1,12 +1,15 @@
 let form = document.getElementById('form-inmar');
 form.addEventListener('submit', e => {
     e.preventDefault();
+
+    let timeStamp = new Date();
+    document.getElementById('time').innerHTML= timeStamp;
+    document.getElementById('time').value = timeStamp;
+
     fetch(form.action, {
         method: 'POST',
         body: new FormData(document.getElementById('form-inmar')),
     }).then (
         response => response.json()
-    ).then ((html) => {
-        alert(success)
-    });
+    );
 });
